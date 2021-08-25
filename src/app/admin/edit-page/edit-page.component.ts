b962382265c7bb177851e81f0aa2ee98858903f4
Abdul Kader Itani <itani.aboudi@gmail.com>
2021-08-25 14:12:54 +0300

    [+] add upate post in firebase db

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,37 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {switchMap} from 'rxjs/operators';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 import {PostsService} from '../../shared/posts.service';
 import {Post} from '../../shared/interfaces';
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-edit-page',
-  templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+    selector: 'app-edit-page',
+    templateUrl: './edit-page.component.html',
+    styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
-  form: FormGroup;
+    form: FormGroup;
+    post: Post;
+    submitted = false;
 
-  constructor(
-      private route: ActivatedRoute,
-      private postsService: PostsService
-  ) { }
+    uSub: Subscription;
 
-  ngOnInit() {
-    this.route.params
-        .pipe(
-            switchMap((params: Params) => {
-              return this.postsService.getById(params['id']);
-            })
-        ).subscribe((post: Post) => {
-          this.form = new FormGroup({
-            title: new FormControl(post.title, Validators.required),
-            text: new FormControl(post.text, Validators.required)
-          });
+    constructor(
+        private route: ActivatedRoute,
+        private postsService: PostsService
+    ) {
+    }
+
+    ngOnInit() {
+        this.route.params
+            .pipe(
+                switchMap((params: Params) => {
+                    return this.postsService.getById(params['id']);
+                })
+            ).subscribe((post: Post) => {
+            this.post = post;
+            this.form = new FormGroup({
+                title: new FormControl(post.title, Validators.required),
+                text: new FormControl(post.text, Validators.required)
+            });
         });
-  }
+    }
+
+    ngOnDestroy() {
+        if (this.uSub) {
+            this.uSub.unsubscribe();
+        }
+    }
+
+    submit() {
+        if (this.form.invalid) {
+            return;
+        }
+        this.uSub = this.postsService.upadate({
+            ...this.post,
+            text: this.form.value.text,
+            title: this.form.value.title
+        }).subscribe(() => {
+            this.submitted = false;
+        });
+    }
 
 }
